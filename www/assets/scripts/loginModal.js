df2d2dcf783ec27c297059615a2ce793b0a54480
Junior Brito <junior.br.pr@gmail.com>
2022-10-24 02:14:33 +0200

    Base JS for the login modal Minor modal styling adjustments

---
+++ www/assets/scripts/loginModal.js
@@ -0,0 +1,18 @@
+const loginModal = document.querySelector(".login-container");
+const modalOpenBtn = document.querySelector(".login-btn");
+const modalCloseBtn = document.querySelector(".login-close");
+
+//When the login button is clicked, open the modal
+modalOpenBtn.addEventListener("click", () => {
+    loginModal.style.display = "block";
+});
+//When the cross on the modal is clicked, close the modal
+modalCloseBtn.addEventListener("click", () => {
+    loginModal.style.display = "none";
+});
+//If the user clicks outside the modal, close the modal
+window.addEventListener("click", (event) => {
+    if (event.target === loginModal) {
+        loginModal.style.display = "none";
+    }
+});
