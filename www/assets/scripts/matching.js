c5694d95f4a9509d62ed86d6bb54935ee574f277
Junior Brito <junior.br.pr@gmail.com>
2022-10-25 02:32:45 +0200

    Started adding responsiveness to the matching page. Some minor css changes. Added the buddy profile modal to the matching page Added javascript functionality to the profile modal Added comments to the matching.js file Added links to buttons on the index page. Added corendon links on buddy "book trip buttons"

--- www/assets/scripts/matching.js
+++ www/assets/scripts/matching.js
@@ -1,11 +1,15 @@
 const buddyList = document.querySelector(".matching-matches");
 const matchingHeaderTitleText = document.querySelector(".matching-header-text");
 
+const buddyProfile = document.querySelector(".popup-container");
+const buddyProfileCloseBtn = document.querySelector(".login-close");
+
 const existingBtn = document.querySelector(".existing-btn");
 const suggestedBtn = document.querySelector(".suggested-btn");
 const incomingBtn = document.querySelector(".incoming-btn");
 const outgoingBtn = document.querySelector(".outgoing-btn");
 
+//Mock data for prototyping purposes
 const data = [
     {
         type: "existing",
@@ -113,31 +117,48 @@ const data = [
     },
 ];
 
+//Populates the list when loading the matching page, "existing" buddy's by default
 populateList(buddyList, "existing", data);
 
+//Button to switch to the list of existing buddy's
 existingBtn.addEventListener("click", () => {
     matchingHeaderTitleText.innerHTML = "Corenbuddy’s";
     populateList(buddyList, "existing", data);
 });
 
+//Button to switch to the list of suggested buddy'
 suggestedBtn.addEventListener("click", () => {
     matchingHeaderTitleText.innerHTML = "Voorgestelde Corenbuddy’s";
     populateList(buddyList, "suggested", data);
 })
 
+//Button to switch to the list of incoming buddy requests
 incomingBtn.addEventListener("click", () => {
     matchingHeaderTitleText.innerHTML = "Inkomendende buddy verzoeken";
     populateList(buddyList, "incoming", data);
 })
 
+//Button to switch to the list of outgoing buddy requests
 outgoingBtn.addEventListener("click", () => {
     matchingHeaderTitleText.innerHTML = "Uitgaande buddy verzoeken";
     populateList(buddyList, "outgoing", data);
 })
 
+//Button to close the "watch profile" modal
+buddyProfileCloseBtn.addEventListener("click", () => {
+    buddyProfile.style.display = "none";
+})
 
-
+/**
+ * For every 'buddy' in the data it makes a list item the buddy's information.
+ * @param {Element} buddyList A unorderedlist element which will be the parent of
+ * the buddy list items.
+ * @param {string} type The type of buddy's you want to populate the list with
+ * ("existing" || "suggested" || "incoming" || "outgoing").
+ * @param {Object[]} data An array with buddy object from which to get the data.
+ */
 function populateList(buddyList, type, data) {
+    //Clear the list before populating again.
     buddyList.innerHTML = "";
 
     data.forEach(buddy => {
@@ -145,21 +166,29 @@ function populateList(buddyList, type, data) {
             const buddyListItem = document.createElement("li");
             buddyListItem.className = "buddy-list-item";
 
+            const buddyAttributeContainer = document.createElement("div");
+            buddyAttributeContainer.className = "buddy-attr-container"
+            buddyListItem.appendChild(buddyAttributeContainer);
+
+            //Add a profile picture to the buddy
             const buddyImg = document.createElement("img");
             buddyImg.className = "buddy-img";
             buddyImg.src = buddy.profilePicture;
-            buddyListItem.appendChild(buddyImg);
+            buddyAttributeContainer.appendChild(buddyImg);
 
+            //Add the buddy's name
             const buddyName = document.createElement("span");
             buddyName.className = "buddy-name buddy-property";
             buddyName.innerHTML = buddy.name;
-            buddyListItem.appendChild(buddyName);
+            buddyAttributeContainer.appendChild(buddyName);
 
+            //Add the buddy's preferred travelling destination
             const destination = document.createElement("span");
             destination.className = "buddy-destination buddy-property";
             destination.innerHTML = buddy.destination;
-            buddyListItem.appendChild(destination);
+            buddyAttributeContainer.appendChild(destination);
 
+            //Add the buddy's timeFrame, convert timeFrame to weeks if possible
             const timeFrame = document.createElement("span");
             timeFrame.className = "buddy-time-frame buddy-property";
             if (buddy.timeFrame % 7 === 0) {
@@ -167,25 +196,37 @@ function populateList(buddyList, type, data) {
             } else {
                 timeFrame.innerHTML = buddy.timeFrame + " dagen";
             }
-            buddyListItem.appendChild(timeFrame);
+            buddyAttributeContainer.appendChild(timeFrame);
 
+            //Add the amount of common interests between the buddy and the user
             const interests = document.createElement("span");
             interests.className = "buddy-common-interests buddy-property";
             interests.innerHTML = buddy.commonInterests;
-            buddyListItem.appendChild(interests);
+            buddyAttributeContainer.appendChild(interests);
 
+            //Add the container which will hold the buttons for the buddy list item
             const btnContainer = document.createElement("div");
             btnContainer.className = "buddy-btn-container";
             buddyListItem.appendChild(btnContainer);
 
+            //Add a "see profile" button to the buddy
             const buddyProfileBtn = document.createElement("button");
             buddyProfileBtn.className = "btn buddy-profile-btn";
             const buddyProfileBtnText = document.createElement("span");
             buddyProfileBtnText.className = "btn-text";
             buddyProfileBtnText.innerHTML = "Profiel bekijken";
+
+            //Onclick make the buddy profile modal visible
+            buddyProfileBtn.addEventListener("click", () => {
+                buddyProfile.style.display = "block";
+            });
             buddyProfileBtn.appendChild(buddyProfileBtnText);
             btnContainer.appendChild(buddyProfileBtn);
 
+            /**
+             * If buddy type = "existing", add a "book a trip" button to the buddy
+             * and also add a "delete buddy" button to the buddy list item.
+             */
             if (buddy.type === "existing"){
                 const bookTripBtn = document.createElement("button");
                 bookTripBtn.className = "btn buddy-book-btn";
@@ -193,6 +234,10 @@ function populateList(buddyList, type, data) {
                 bookTripBtnText.className = "btn-text";
                 bookTripBtnText.innerHTML = "Boek een reis!";
                 bookTripBtn.appendChild(bookTripBtnText);
+                //The "book a trip" button redirects to the corendon website.
+                bookTripBtn.addEventListener("click", () => {
+                    location.href = "www.corendon.nl";
+                });
                 btnContainer.appendChild(bookTripBtn);
 
                 const buddyDeleteBtn = document.createElement("button");
@@ -204,6 +249,10 @@ function populateList(buddyList, type, data) {
                 btnContainer.appendChild(buddyDeleteBtn);
             }
 
+            /**
+             * If buddy type = "suggested" add a "send buddy request" button to the
+             * buddy list item.
+             */
             if (buddy.type === "suggested"){
                 const sendRequestBtn = document.createElement("button");
                 sendRequestBtn.className = "btn buddy-send-request-btn";
@@ -214,6 +263,10 @@ function populateList(buddyList, type, data) {
                 btnContainer.appendChild(sendRequestBtn);
             }
 
+            /**
+             * If buddy type = "incoming" add an "accept buddy request" button  and a
+             * refuse buddy request button to the buddy list item.
+             */
             if (buddy.type === "incoming"){
                 const acceptRequestBtn = document.createElement("button");
                 acceptRequestBtn.className = "btn buddy-accept-request-btn";
@@ -232,6 +285,10 @@ function populateList(buddyList, type, data) {
                 btnContainer.appendChild(refuseRequestBtn);
             }
 
+            /**
+             * If buddy type = "outgoing" add a "withdraw buddy request" button
+             * to the buddy list item.
+             */
             if (buddy.type === "outgoing"){
                 const withdrawRequestBtn = document.createElement("button");
                 withdrawRequestBtn.className = "btn btn-red buddy-refuse-request-btn";
@@ -242,6 +299,7 @@ function populateList(buddyList, type, data) {
                 btnContainer.appendChild(withdrawRequestBtn);
             }
 
+            //Finally, add the buddy list item to the buddy unordered-list.
             buddyList.appendChild(buddyListItem);
         }
     })
