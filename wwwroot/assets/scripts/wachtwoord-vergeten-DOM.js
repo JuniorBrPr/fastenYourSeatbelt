7c2e02615fb3b05595713b29a6180b623744950e
Jurian Blommers <jurian.blommers@hva.nl>
2022-11-22 19:19:48 +0100

    javascript werkende knop toegevoegd

--- wwwroot/assets/scripts/wachtwoord-vergeten-DOM.js
+++ wwwroot/assets/scripts/wachtwoord-vergeten-DOM.js
@@ -2,11 +2,13 @@ window.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
     console.log("als je dit leest ben je homo");
+    document.getElementById("pagina2").style.display = "none";
 }
 
 
 function hansklok(){
     document.getElementById("pagina1").style.display = "none";
+    document.getElementById("pagina2").style.display = "flex";
 
     console.log("uitgevoerd");
 
