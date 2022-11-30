61c3b5b8f2dc5600c1dfd3cf4e6ad9465c999c43
Jurian Blommers <jurian.blommers@hva.nl>
2022-11-30 13:34:32 +0100

    3 pagina swap compleet

--- wwwroot/assets/scripts/wachtwoord-vergeten-DOM.js
+++ wwwroot/assets/scripts/wachtwoord-vergeten-DOM.js
@@ -1,16 +1,42 @@
 window.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
-    console.log("als je dit leest ben je homo");
     document.getElementById("pagina2").style.display = "none";
+    document.getElementById("pagina3").style.display = "none";
+    document.getElementById("wwvg-knop").addEventListener("click", evt => showpage2(evt));
+    console.log("hoi");
+    document.getElementById("wwvg-knop2").addEventListener("click", evt => showpage3(evt));
+
+    document.getElementById("wwvg-knop3").addEventListener("click", evt => validateForm(evt));
+
 }
 
+function showpage1(evt){
+    evt.preventDefault();
+    document.getElementById("pagina1").style.display = "flex";
+    document.getElementById("pagina2").style.display = "none";
+    document.getElementById("pagina3").style.display = "none";
+    console.log("uitgevoerd");
+}
 
-function hansklok(){
+function showpage2(evt){
+    evt.preventDefault();
     document.getElementById("pagina1").style.display = "none";
     document.getElementById("pagina2").style.display = "flex";
-
+    document.getElementById("pagina3").style.display = "none";
     console.log("uitgevoerd");
-
 }
-document.getElementById("wwvg-knop").addEventListener("click", evt => hansklok());
+
+function showpage3(evt){
+    evt.preventDefault();
+    document.getElementById("pagina1").style.display = "none";
+    document.getElementById("pagina2").style.display = "none";
+    document.getElementById("pagina3").style.display = "flex";
+    console.log("uitgevoerd");
+}
+
+function validateForm(evt) {
+    evt.preventDefault();
+    let x = document.forms["nieuw-wachtwoord-form"]["nieuw-wachtwoord"];
+    console.log(x);
+}
