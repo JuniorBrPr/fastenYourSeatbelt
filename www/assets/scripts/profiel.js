10538f554468a6820d19334300895562efebaadf
Nizar Amine <nizar.amine@hva.nl>
2022-12-05 15:12:56 +0100

    Push for development

--- www/assets/scripts/profiel.js
+++ www/assets/scripts/profiel.js
@@ -1,99 +1,125 @@
 import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
 
-window.addEventListener("DOMContentLoaded", function () {
-	dataLoad();
-});
+const form = document.querySelector(".profiel")
+const subBtn = document.querySelector('.saveBtn');
+window.addEventListener('DOMContentLoaded', async () => await dataLoad());
+// subBtn.addEventListener('Submit', function(e) {submitData()});
 
 const userId = FYSCloud.Session.get("userId", 1);
 
 console.log(userId);
 
+
 async function dataLoad() {
-	/*Setting Field vars*/
 
-	const firstNameField = document.getElementById("voornaam");
-	const lastNameField = document.getElementById("achternaam");
-	const bioField = document.getElementById("bio");
-	const birthDateField = document.getElementById("gdatum");
-	const interField = document.getElementsByClassName("inter-field");
-	const destinationField = document.getElementById("bestemming");
-	const tijdsbestekStart = document.getElementById("startdate");
-	const tijdsbestekEnd = document.getElementById("enddate");
+    /*Setting Field vars*/
 
-	/* Pulling Data and parsing it into the fields*/
+    const firstNameField = document.getElementById('voornaam')
+    const lastNameField = document.getElementById('achternaam')
+    const bioField = document.getElementById('bio')
+    const birthDateField = document.getElementById('gdatum')
+    const interField = document.getElementsByClassName('inter-field')
+    const destinationField = document.getElementById('bestemming')
+    const tijdsbestekStart = document.getElementById('startdate')
+    const tijdsbestekEnd = document.getElementById('enddate')
+    const formField = document.getElementsByClassName('profiel')
 
-	const getData = await FYSCloud.API.queryDatabase(
-		"SELECT first_name AS firstName,\n" +
-			"       last_name AS lastName,\n" +
-			"       birthdate as bday,\n" +
-			"       biography as bio,\n" +
-			"       start_date as startDate,\n" +
-			"       end_date as endDate,\n" +
-			"       destination,\n" +
-			"       budget\n" +
-			"FROM user\n" +
-			"LEFT JOIN profile p on p.profile_id = ?\n" +
-			"WHERE user_id = ?",
-		[userId, userId]
-	);
 
-	console.log(getData);
+    /* Pulling Data and parsing it into the fields*/
 
-	loadData(getData);
+    const getData = await FYSCloud.API.queryDatabase(
+        "SELECT first_name AS firstName,\n" +
+        "       last_name AS lastName,\n" +
+        "       birthdate as bday,\n" +
+        "       biography as bio,\n" +
+        "       start_date as startDate,\n" +
+        "       end_date as endDate,\n" +
+        "       destination,\n" +
+        "       budget\n" +
+        "FROM user\n" +
+        "INNER JOIN profile p on p.profile_id = ?\n" +
+        "WHERE user_id = ?", [userId, userId]);
 
-	/*Load all data in the right fields on Profile page*/
+    console.log(getData);
 
-	function loadData(data) {
-		firstNameField.value = data[0].firstName;
-		lastNameField.value = data[0].lastName;
-		bioField.value = data[0].bio;
-		destinationField.value = data[0].destination;
+    loadData(getData);
 
-		const bday = data[0].bday;
+    /*Load all data in the right fields on Profile page*/
 
-		//get date
-		const date = new Date(bday);
-		//get year of a date
-		const bdayYear = date.getFullYear();
-		/**
-		 * get month gives the month starting from 0 going to 11
-		 * so to make january the fist month add 1,
-		 * also you want to make sure it is always formatted MM so 2 should become 02,
-		 * padStart autofills left over space so the length should be 2 and if it is empty fill it with a zero
-		 * padStart is a function on a string, and getMonth gives an int, so you need to parse it to a string
-		 *
-		 * remove this comment later
-		 */
-		const bdayMonth = (date.getMonth() + 1).toString().padStart(2, 0);
-		// same logic as month only getDate weirdly does start at 1
-		const bdayDay = date.getDate().toString().padStart(2, 0);
-		//correct format for date input
-		const dateFormat = `${bdayYear}-${bdayMonth}-${bdayDay}`;
-		birthDateField.value = dateFormat;
-		/** TODO
-		 * I personally would create a function that converts the date in the correct format,
-		 * using the code above
-		 */
-		// tijdsbestekStart.value = data[0].startDate;
-		// tijdsbestekEnd.value = data[0].endDate;
-	}
+    function loadData(data) {
+        firstNameField.value = data[0].firstName;
+        lastNameField.value = data[0].lastName;
+        bioField.value = data[0].bio;
+        destinationField.value = data[0].destination;
+
+        const bday = data[0].bday;
+        const tijdEnd = data[0].endDate;
+        const tijdStart = data[0].startDate;
+
+        const birthday = change(bday)
+        const tijdEndDate = change(tijdEnd)
+        const tijdStartDate = change(tijdStart)
+
+        birthDateField.value = birthday;
+        tijdsbestekStart.value = tijdEndDate;
+        tijdsbestekEnd.value = tijdStartDate;
+
+    }
+
+
+    function change(date) {
+
+        const changeDate = new Date(date)
+        const getYear = changeDate.getFullYear();
+        const getMonth = (changeDate.getMonth() + 1).toString().padStart(2, 0);
+        // same logic as month only getDate weirdly does start at 1
+        const getDay = changeDate.getDate().toString().padStart(2, 0);
+        //correct format for date input
+        return `${getYear}-${getMonth}-${getDay}`;
+    }
 }
 
 /*Submit data to the databse*/
 
-function submitData(data) {
-	const InsertData = FYSCloud.API.queryDatabase(
-		"INSERT first_name AS firstName,\n" +
-			"       last_name AS lastName,\n" +
-			"       birthdate as bday,\n" +
-			"       biography as bio,\n" +
-			"       start_date as startDate,\n" +
-			"       end_date as endDate,\n" +
-			"       destination,\n" +
-			"       budget\n" +
-			"FROM user\n" +
-			"LEFT JOIN profile p on p.profile_id = ?\n" +
-			"WHERE user_id = ?",
-		[userId, userId]
-	);
+
+async function submitData(data) {
+
+    data.bday = FYSCloud.Utils.toSqlDatetime(new Date(data.bday))
+    data.startDate = FYSCloud.Utils.toSqlDatetime(new Date(data.startDate))
+    data.endDate = FYSCloud.Utils.toSqlDatetime(new Date(data.endDate))
+
+
+    const updateUserData = await FYSCloud.API.queryDatabase(
+        "UPDATE user\n" +
+        "SET    first_name = ?,\n" +
+        "       last_name = ?\n" +
+        "WHERE user_id = ?", [data.firstName, data.lastName, userId]);
+
+    const updateProfileData = await FYSCloud.API.queryDatabase(
+        "UPDATE profile\n" +
+        "SET    birthdate = ?,\n" +
+        "       biography = ?,\n" +
+        "       start_date = ?,\n" +
+        "       end_date = ?,\n" +
+        "       destination = ?,\n" +
+        "       budget = ?\n" +
+        "WHERE profile_id = ?;", [data.bday, data.bio,  data.startDate, data.endDate, data.destination, data.budget,
+            userId]);
+
+    console.log(updateProfileData)
+    console.log(updateUserData)
 }
+
+function createObject() {
+    return {
+        firstName: document.querySelector('#voornaam').value,
+        lastName: document.getElementById('achternaam').value,
+        bio: document.getElementById('bio').value,
+        birthDate: document.getElementById('gdatum').value,
+        destination: document.getElementById('bestemming').value,
+        tijdsbestekStart: document.getElementById('startdate').value,
+        tijdsbestekEnd: document.getElementById('enddate').value
+    }
+}
+
+console.log(createObject())
