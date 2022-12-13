698b7f77d8a45c0736f84853d6e6af684cc7b410
Tim Knops <tim.knops@hva.nl>
2022-12-13 08:54:53 +0100

    Code cleanup in contact.js

--- wwwroot/assets/js/pages/contact.js
+++ wwwroot/assets/js/pages/contact.js
@@ -33,15 +33,13 @@ form.addEventListener("submit", async (e) => {
  * @returns {object} object of the contact form input values.
  */
 function createObject(name, phone, email, subject, message) {
-    const object = {
+    return {
         name: name.value,
         phone: phone.value,
         email: email.value,
         subject: subject.value,
         message: message.value
     };
-
-    return object;
 }
 
 /**
@@ -50,7 +48,7 @@ function createObject(name, phone, email, subject, message) {
  * @param {object} input - object of all the contact form values.
  */
 async function sendEmail(input) {
-    const emailRecipiants = [
+    const emailRecipients = [
         {address: "lucas.meester@hva.nl"}, 
         {address: "tim.knops@hva.nl"},
         {address: "junior.brito.perez@hva.nl"},
@@ -64,9 +62,9 @@ async function sendEmail(input) {
     await FYSCloud.API.sendEmail({
         from: {
             name: input.name,
-            adress: input.email
+            address: input.email
         },
-        to: emailRecipiants, 
+        to: emailRecipients,
         subject: input.subject,
         priority: "high",
         html: `<p>${input.message}</p>
