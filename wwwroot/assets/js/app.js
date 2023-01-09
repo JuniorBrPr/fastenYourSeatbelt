85dc43c5b734f55688c891b8fca7f690ccf8f92f
Samed Dalkilic <samed.dalkilic@hva.nl>
2023-01-09 02:03:30 +0100

    geldeenheid toegevoegd + ww vergeten en profiel pagina responsive

--- wwwroot/assets/js/app.js
+++ wwwroot/assets/js/app.js
@@ -1344,3 +1344,38 @@ document.querySelector("#localizationLanguageSwitch").addEventListener("change",
 	FYSCloud.Localization.switchLanguage(this.value);
 	FYSCloud.Session.set("language", this.value);
 });
+
+var currencyInput = document.querySelector("#budget")
+var currency = 'EUR'
+
+// format inital value
+onBlur({target:currencyInput})
+
+// bind event listeners
+currencyInput.addEventListener('focus', onFocus)
+currencyInput.addEventListener('blur', onBlur)
+
+
+function localStringToNumber( s ){
+	return Number(String(s).replace(/[^0-9.-]+/g,""))
+}
+
+function onFocus(e){
+	var value = e.target.value;
+	e.target.value = value ? localStringToNumber(value) : ''
+}
+
+function onBlur(e){
+	var value = e.target.value
+
+	var options = {
+		maximumFractionDigits : 2,
+		currency              : currency,
+		style                 : "currency",
+		currencyDisplay       : "symbol"
+	}
+
+	e.target.value = (value || value === 0)
+		? localStringToNumber(value).toLocaleString(undefined, options)
+		: ''
+}
