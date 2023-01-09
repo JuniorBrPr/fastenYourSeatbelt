98a8c964e30eab8f7a24e969c4c8d689ac6cdc9c
Samed Dalkilic <samed.dalkilic@hva.nl>
2023-01-09 13:22:37 +0100

    changes to profile page

--- wwwroot/assets/js/app.js
+++ wwwroot/assets/js/app.js
@@ -1344,39 +1344,3 @@ document.querySelector("#localizationLanguageSwitch").addEventListener("change",
 	FYSCloud.Localization.switchLanguage(this.value);
 	FYSCloud.Session.set("language", this.value);
 });
-
-//currency
-var currencyInput = document.querySelector("#budget")
-var currency = 'EUR'
-
-// format inital value
-onBlur({target:currencyInput})
-
-// bind event listeners
-currencyInput.addEventListener('focus', onFocus)
-currencyInput.addEventListener('blur', onBlur)
-
-//currency regex checker
-function localStringToNumber( s ){
-	return Number(String(s).replace(/[^0-9.-]+/g,""))
-}
-
-function onFocus(e){
-	var value = e.target.value;
-	e.target.value = value ? localStringToNumber(value) : ''
-}
-
-function onBlur(e){
-	var value = e.target.value
-
-	var options = {
-		maximumFractionDigits : 2,
-		currency              : currency,
-		style                 : "currency",
-		currencyDisplay       : "symbol"
-	}
-
-	e.target.value = (value || value === 0)
-		? localStringToNumber(value).toLocaleString(undefined, options)
-		: ''
-}
